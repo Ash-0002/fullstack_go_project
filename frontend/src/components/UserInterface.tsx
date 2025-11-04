3a0d14c98921765523721cf1b68ddabb3800f866
Ashish <ashish.rajput@livekeeping.com>
2025-11-04 14:01:09 +0530

    frontend

---
+++ frontend/src/components/UserInterface.tsx
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CardComponent from "./CardComponent";
+
+interface User {
+    id: number;
+    name: string;
+    email: string;
+}
+
+interface UserInterfaceProps {
+    backendName: string; // go
+}
+
+const UserInterface: React.FC<UserInterfaceProps> = ({ backendName }) => {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080";
+    const [users, setUsers] = useState<User[]>([]);
+    const [newUser, setNewUser] = useState({name: '', email: ""});
+    const [updateUser, setUpdateUser] = useState({id: '', name: '', email: ''});
+    //Define styles based on the backedn name 
+    const backgroundColors : { [key: string]: string } = {
+        go: 'bg-cyan-500'
+    }
+    
+    const buttonColors: { [key: string]: string } = {  
+        go: 'bg-cyan-700 hover:bg-blue-600'
+    }
+    
+    const bgColor = backgroundColors[backendName as keyof typeof backgroundColors] || 'bg-gray-200';
+    const btnColor = buttonColors[backendName as keyof typeof buttonColors] || 'bg-gray-700 hover:bg-gray-600';
+    
+    // fetch all users
+    
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const response = await axios.get(`${apiUrl}/api/${backendName}/users`);
+                setUsers(response.data.reverse());
+            } catch (error) {
+                console.error("Error fetching users:", error);
+            }
+        };
+        fetchUsers();       
+    },[backendName, apiUrl]);
+
+    // Create a new User 
+
+
+    const createUser = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        try{
+            const response = await axios.post(`${apiUrl}/api/${backendName}/users`, newUser);
+            setUsers([response.data, ...users]);
+            setNewUser({name: '', email: ''});
+        } catch (error) {
+            console.error("Error creating user:", error);
+        }
+    }
+
+
+
+    return (
+        <div className={`user-interface ${bgColor} ${backendName} w-full max-w-md p-4 my-4 rounded shadow`}>
+            <img src={`/${backendName}logo.svg`} alt={`${backendName} logo`} className="w-20 h-20 mb-6 mx-auto" />
+            <h2 className="text-xl font-bold text-center text-white mb-2">{`${backendName.charAt(0).toUpperCase() + backendName.slice(1)} Backend`}</h2>
+
+        {/* create user  */}
+
+        <form onSubmit={createUser} className="mb-6 p-4 bg-blue-100 rounded shadow text-black">
+            <input type="text"
+                placeholder="Name"
+                value={newUser.name}
+                onChange={(e) => setNewUser({ ...newUser, name: e.target.value})}
+                className="mb-2 w-full p-2 border border-gray-300 rounded"
+            />
+            <input type="text"
+                placeholder="Email"
+                value={newUser.email}
+                onChange={(e) => setNewUser({ ...newUser, email: e.target.value})}
+                className="mb-2 w-full p-2 border border-gray-300 rounded"
+            />
+            <button className="w-full p-2 text-white bg-blue-500 rounded hover:bg-blue-600">
+                Add User
+            </button>
+        </form>
+
+
+
+
+           {/* display users  */}
+            <div className="space-y-4">
+                {users.map((user) => (
+                    <div key={user.id} className="flex item-center justify-between bg-white p-4 rounded-lg shadow">
+                        <CardComponent card={user} />
+                        <button onClick={() => deleteUser(user.id)} className={`${btnColor} text-white py-2 px-4 rounded`}>
+                            Delete User
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+};
+
+ 
+export default UserInterface;
+
+ 
